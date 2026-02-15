f1a648caba8a753f31b6ece290497da918057f50
Mett3zmod <matt3d.design@gmail.com>
2026-02-15 20:02:54 +0100

    1.21.1-Fabric

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p align="center"><img src="src/main/resources/logo.png" alt="Logo" width="200"></p>
+<p align="center"><img src="src/main/resources/assets/artifactscrafting/icon.png" alt="Logo" width="200"></p>
 <h1 align="center">Artifacts Crafting  <br>
     <br><br>
 </h1>
--- src/main/java/net/mett3z/artifactscrafting/ArtifactsCrafting.java
+++ src/main/java/net/mett3z/artifactscrafting/ArtifactsCrafting.java
@@ -1,64 +1,16 @@
 package net.mett3z.artifactscrafting;
 
+import net.fabricmc.api.ModInitializer;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.mojang.logging.LogUtils;
+public class ArtifactsCrafting implements ModInitializer {
+	public static final String MOD_ID = "artifactscrafting";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
+	@Override
+	public void onInitialize() {
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ArtifactsCrafting.MOD_ID)
-public class ArtifactsCrafting {
-    public static final String MOD_ID = "artifactscrafting";
-    public static final Logger LOGGER = LogUtils.getLogger();
-
-    public ArtifactsCrafting(IEventBus modEventBus, ModContainer modContainer) {
-
-        modEventBus.addListener(this::commonSetup);
-
-        NeoForge.EVENT_BUS.register(this);
-
-        modEventBus.addListener(this::addCreative);
-
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    private void commonSetup(FMLCommonSetupEvent event) {
-
-    }
-
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
+	}
 }
