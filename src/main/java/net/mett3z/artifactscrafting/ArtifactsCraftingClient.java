20d310707cae197faedf6352a557fc8b39a491ff
Mett3zmod <matt3d.design@gmail.com>
2026-02-15 19:42:56 +0100

    1.20.1-Fabric

---
+++ src/main/java/net/mett3z/artifactscrafting/ArtifactsCraftingClient.java
@@ -0,0 +1,10 @@
+package net.mett3z.artifactscrafting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArtifactsCraftingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
