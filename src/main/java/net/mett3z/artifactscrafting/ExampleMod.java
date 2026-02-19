d1f6e2b36d54950e864c095602a01c32d0e2df56
Mett3zmod <matt3d.design@gmail.com>
2026-02-19 20:46:53 +0100

    19/02/2026 Commit 1

--- src/main/java/net/mett3z/artifactscrafting/ExampleMod.java
+++ src/main/java/net/mett3z/artifactscrafting/ExampleMod.java
@@ -15,7 +15,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod
 {
@@ -37,25 +36,21 @@ public class ExampleMod
     private void commonSetup(final FMLCommonSetupEvent event){
 
     }
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
-            // Some client setup code
             LOGGER.info("HELLO FROM CLIENT SETUP");
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
