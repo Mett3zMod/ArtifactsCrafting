080300d3f07781dbc4b10c7cdaee7b4d85369d99
Mett3z <matt3d.design@gmail.com>
2026-08-07 23:28:19 +0200

    Initial commit

--- src/main/java/net/mett3z/artifactscrafting/ArtifactsCrafting.java
+++ src/main/java/net/mett3z/artifactscrafting/ArtifactsCrafting.java
@@ -1,58 +1,29 @@
 package net.mett3z.artifactscrafting;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
-@Mod(ArtifactsCrafting.MOD_ID)
+@Mod(modid = ArtifactsCrafting.MOD_ID, name = ArtifactsCrafting.NAME, version = ArtifactsCrafting.VERSION,
+     dependencies = "required-after:artifacts")
 public class ArtifactsCrafting
 {
     public static final String MOD_ID = "artifactscrafting";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    public ArtifactsCrafting(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
+    public static final String NAME = "Artifacts Crafting";
+    public static final String VERSION = "1.0.0-1.12.2";
 
-        modEventBus.addListener(this::commonSetup);
+    private static Logger logger;
 
-        MinecraftForge.EVENT_BUS.register(this);
-
-        modEventBus.addListener(this::addCreative);
-
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event){
-
-    }
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
     {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
     }
 }
